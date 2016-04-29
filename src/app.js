17c87ed3f811477870c27f0f3ee337fece481181
Kluth, Nathan <nlkluth@gmail.com>
2016-04-29 17:36:36 -0600

    stubbin

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginContainer from './components/login/LoginContainer';
+
+const target = document.getElementById('root');
+ReactDOM.render(LoginContainer, target);
---
+++ src/components/login/LoginContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LoginContainer extends React.Component {
+  render() {
+    return (
+      <div>Login</div>
+    );
+  }
+}
+
+export default LoginContainer;
