7c3aca696a028d09f583de22e691cf25b223073e
Kluth, Nathan <nlkluth@gmail.com>
2016-05-02 13:47:27 -0600

    config

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,24 @@
-webpack.config.js
+const config = {
+  entry: ['src/app.js'],
+  output: {
+    filename: '[name].[hash].js',
+    path: 'dist',
+    publicPath: '/'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      hash: false,
+      filename: 'index.html',
+      inject: 'body',
+      minify: {
+        collapseWhitespace: true
+      }
+    })
+  ],
+  module: {
+    loaders: []
+  }
+};
+
+module.exports = config;
