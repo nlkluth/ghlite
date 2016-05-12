8a7ce12def3ff9892c29412ac79818117bb5fce7
Kluth, Nathan <nlkluth@gmail.com>
2016-05-12 13:36:18 -0600

    connect

--- server/config/server.js
+++ server/config/server.js
@@ -1,5 +1,10 @@
 const routes = require('../routes');
 
 exports.setupServer = (config, server) => {
+  server.connection({
+    port: process.env.PORT || 8000,
+    host: '0.0.0.0'
+  });
+
   routes(server);
 };
